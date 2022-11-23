c1ede6f53a9bc69dd5b4f49acb478d32e6052aff
jakobkukla <jakob.kukla@gmail.com>
2022-11-23 22:07:53 +0100

    Add biber and biblatex-apa to latex packages

--- home/texlive.nix
+++ home/texlive.nix
@@ -4,7 +4,8 @@
   home-manager.users.jakob = {
     home.packages = with pkgs; [
       #texlive.combined.scheme-medium
-      (texlive.combine { inherit (texlive) scheme-medium biblatex; })
+      (texlive.combine { inherit (texlive) scheme-medium biblatex biblatex-apa; })
+      biber
     ];
   };
 }
