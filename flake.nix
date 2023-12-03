a7c176576b6f04db60b719427a21d1e04122c3ba
jakobkukla <jakob.kukla@gmail.com>
2023-12-03 02:34:38 +0100

    base: switch to stable until vscode and freezes on start are fixed

--- flake.nix
+++ flake.nix
@@ -2,8 +2,8 @@
   description = "NixOS system config";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    home-manager.url = "github:nix-community/home-manager/release-23.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     impermanence.url = "github:nix-community/impermanence";
     agenix.url = "github:ryantm/agenix";
