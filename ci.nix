2a26dc3fc9257b68678c16b92ee2dc60782bb350
jakobkukla <jakob.kukla@gmail.com>
2024-03-19 13:16:48 +0100

    ci: use self-hosted attic cache instead of cachix

--- ci.nix
+++
@@ -1,6 +0,0 @@
-let
-  outputs = builtins.getFlake (toString ./.);
-  pkgs = outputs.inputs.nixpkgs;
-  drvs = pkgs.lib.collect pkgs.lib.isDerivation outputs.nixosConfigurations.matebook.config.system.build.toplevel;
-in
-  drvs
