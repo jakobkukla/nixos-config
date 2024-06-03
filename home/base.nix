f5a602ea5d2b50414442b521b3da80131328a830
jakobkukla <jakob.kukla@gmail.com>
2024-06-03 18:33:25 +0200

    base: move home-manager config to user module

--- home/base.nix
+++ home/base.nix
@@ -12,18 +12,5 @@
 
     # FIXME: where to put this? currently all defaultApps declarations live in the firefox module
     xdg.mimeApps.enable = true;
-
-    # This value determines the Home Manager release that your
-    # configuration is compatible with. This helps avoid breakage
-    # when a new Home Manager release introduces backwards
-    # incompatible changes.
-    #
-    # You can update Home Manager without changing this value. See
-    # the Home Manager release notes for a list of state version
-    # changes in each release.
-    home.stateVersion = "21.11";
-
-    # Let Home Manager install and manage itself.
-    programs.home-manager.enable = true;
   };
 }
