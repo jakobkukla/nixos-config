8f52fb2218b1ebe93f3d1417b59aa5c6b36bcdc9
jakobkukla <jakob.kukla@gmail.com>
2024-06-05 04:30:52 +0200

    secrets: rekey for pc machine

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -3,10 +3,11 @@ let
   users = [jakob];
 
   matebook = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ2PyNHnOUfdYWB0oFjuRZQ98/2biKQVy1jt4+vEAmiT root@nixos-matebook";
+  pc = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFUTEU+53xE6W+LQKsb0/L0Sn4A7c5lQynNF6yCn2I9I root@nixos-pc";
   systems = [matebook];
 in {
-  "root.age".publicKeys = systems;
-  "jakob.age".publicKeys = [jakob] ++ systems;
+  "root.age".publicKeys = users ++ systems;
+  "jakob.age".publicKeys = users ++ systems;
   "netrc-attic.age".publicKeys = users ++ systems;
   "spotify.age".publicKeys = users ++ systems;
 }
