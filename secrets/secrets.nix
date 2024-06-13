4cf3831898b221c3c7105bad7872cafd4389a03b
jakobkukla <jakob.kukla@gmail.com>
2024-06-13 14:27:44 +0200

    secrets: rekey for server machine

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -4,7 +4,8 @@ let
 
   matebook = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ2PyNHnOUfdYWB0oFjuRZQ98/2biKQVy1jt4+vEAmiT root@nixos-matebook";
   pc = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFUTEU+53xE6W+LQKsb0/L0Sn4A7c5lQynNF6yCn2I9I root@nixos-pc";
-  systems = [matebook];
+  server = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPn5mXfQ1OWg70WbZKMttRGXDWRH7sPXGl67k88xSCIp root@nixos-server";
+  systems = [matebook pc server];
 in {
   "root.age".publicKeys = users ++ systems;
   "jakob.age".publicKeys = users ++ systems;
