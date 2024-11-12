155f10211d98d62a15623066680bcfab4a104025
jakobkukla <jakob.kukla@gmail.com>
2024-11-12 03:44:36 +0100

    fix(base): use beaconDB as geoclue provider
    workaround for MLS shutdown. see https://github.com/NixOS/nixpkgs/issues/321121

--- machines/base.nix
+++ machines/base.nix
@@ -1,12 +1,7 @@
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
-{
-  config,
-  pkgs,
-  lib,
-  ...
-}: {
+{pkgs, ...}: {
   # TODO: put this somewhere else
   modules = {
     nix.enable = true;
@@ -18,6 +13,13 @@
 
   # location (needed for gammastep)
   location.provider = "geoclue2";
+
+  # FIXME: workaround for MLS shutdown. See: https://github.com/NixOS/nixpkgs/issues/321121
+  services.geoclue2 = {
+    geoProviderUrl = "https://beacondb.net/v1/geolocate";
+    # submitData = true;
+    # submissionUrl = "https://beacondb.net/v2/geosubmit";
+  };
 
   # Select internationalisation properties.
   i18n.defaultLocale = "de_AT.UTF-8";
