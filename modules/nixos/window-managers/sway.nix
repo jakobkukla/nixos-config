ec4d3633477370af7e81a8aa264885766cab28d0
jakobkukla <jakob.kukla@gmail.com>
2026-03-12 17:16:45 +0100

    fix(sway): remove redundant polkit configuration.

--- modules/nixos/window-managers/sway.nix
+++ modules/nixos/window-managers/sway.nix
@@ -38,24 +38,6 @@ in {
       }
     ];
 
-    # Enable polkit authentication agent
-    environment.systemPackages = with pkgs; [
-      polkit_gnome
-    ];
-    systemd.user.services.polkit-gnome-authentication-agent-1 = {
-      description = "polkit-gnome-authentication-agent-1";
-      wantedBy = ["graphical-session.target"];
-      wants = ["graphical-session.target"];
-      after = ["graphical-session.target"];
-      serviceConfig = {
-        Type = "simple";
-        ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
-        Restart = "on-failure";
-        RestartSec = 1;
-        TimeoutStopSec = 10;
-      };
-    };
-
     home-manager.users.${config.modules.user.name} = {
       wayland.windowManager.sway = {
         enable = true;
