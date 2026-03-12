c517a80d7a163df95d3b12a8131492efd1de7314
jakobkukla <jakob.kukla@gmail.com>
2026-03-12 16:48:37 +0100

    wip replace greetd with dms-greeter

--- modules/nixos/window-managers/default.nix
+++ modules/nixos/window-managers/default.nix
@@ -25,84 +25,72 @@ in {
 
   # FIXME: we allow using multiple WMs (which is nice): but what about WM specific configuration?
   # Say sway enables dunst but niri uses DMS as notification daemon. they will conflict...
-  config = let
-    startCommands = {
-      # FIXME: This is needed to source home.sessionVariables in Hyprland.
-      # Keep track of https://github.com/nix-community/home-manager/issues/2659 for a cleaner solution.
-      hyprland =
-        (pkgs.writeShellScript "hyprland_wrapper" ''
-          source "${config.modules.user.homeDirectory}/.nix-profile/etc/profile.d/hm-session-vars.sh"
+  config = lib.mkMerge [
+    (lib.mkIf cfg.enable {
+      assertions = [
+        {
+          assertion = config.modules.windowManager.${cfg.default}.enable;
+          message = "\"${cfg.default}\" is configured as the default window manager but is not enabled";
+        }
+      ];
 
-          exec ${lib.getExe config.programs.hyprland.package} $@
-        '').outPath;
-    };
-  in
-    lib.mkMerge [
-      (lib.mkIf cfg.enable {
-        assertions = [
-          {
-            assertion = config.modules.windowManager.${cfg.default}.enable;
-            message = "\"${cfg.default}\" is configured as the default window manager but is not enabled";
-          }
+      services.displayManager.dms-greeter = {
+        enable = true;
+        compositor.name = cfg.default;
+        configHome = config.modules.user.homeDirectory;
+      };
+
+      home-manager.users.${config.modules.user.name} = {
+        home.packages = with pkgs; [
+          # clipboard utility
+          wl-clipboard
+          # FIXME: think about replacing with dms
+          # screenshot utility
+          grim
         ];
 
-        modules.greetd = {
+        home.sessionVariables = {
+          # Enable wayland backend for chromium/electron applications
+          NIXOS_OZONE_WL = "1";
+          # Fix java blank-screen issues for xwayland-satellite
+          _JAVA_AWT_WM_NONREPARENTING = "1";
+        };
+
+        home.pointerCursor = {
           enable = true;
-          command = startCommands.${cfg.default};
-          user = config.modules.user.name;
+          package = pkgs.adwaita-icon-theme;
+
+          name = "Adwaita";
+          size = 24;
+
+          x11.enable = true;
+          gtk.enable = true;
         };
 
-        home-manager.users.${config.modules.user.name} = {
-          home.packages = with pkgs; [
-            # clipboard utility
-            wl-clipboard
-            # FIXME: think about replacing with dms
-            # screenshot utility
-            grim
+        # TODO: configure input and output devices here:
+        # libinput
+        # kanshi
+
+        # FIXME: this is not only window manager config anymore. rather a DE? (maybe move to profiles/desktop?)
+        # TODO: think about replacing each of these with DMS
+
+        modules.home.rofi.enable = true;
+
+        services.swayidle = {
+          enable = true;
+          timeouts = [
+            {
+              timeout = 1200;
+              command = "${pkgs.systemd}/bin/systemctl suspend";
+            }
           ];
-
-          home.sessionVariables = {
-            # Enable wayland backend for chromium/electron applications
-            NIXOS_OZONE_WL = "1";
-            # Fix java blank-screen issues for xwayland-satellite
-            _JAVA_AWT_WM_NONREPARENTING = "1";
-          };
-
-          home.pointerCursor = {
-            enable = true;
-            package = pkgs.adwaita-icon-theme;
-
-            name = "Adwaita";
-            size = 24;
-
-            x11.enable = true;
-            gtk.enable = true;
-          };
-
-          # TODO: configure input and output devices here:
-          # libinput
-          # kanshi
-
-          # FIXME: this is not only window manager config anymore. rather a DE? (maybe move to profiles/desktop?)
-          # TODO: think about replacing each of these with DMS
-
-          modules.home.rofi.enable = true;
-
-          services.swayidle = {
-            enable = true;
-            timeouts = [
-              {
-                timeout = 1200;
-                command = "${pkgs.systemd}/bin/systemctl suspend";
-              }
-            ];
-          };
-
-          services.gammastep = {
-            enable = true;
-            provider = "geoclue2";
-          };
         };
-      })
-    ];
+
+        services.gammastep = {
+          enable = true;
+          provider = "geoclue2";
+        };
+      };
+    })
+  ];
 }
