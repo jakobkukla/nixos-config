18c4c11510657545579f73f8d47a95f98bdca676
jakobkukla <jakob.kukla@gmail.com>
2026-03-16 17:13:39 +0100

    wip niri

--- modules/profiles/desktop.nix
+++ modules/profiles/desktop.nix
@@ -13,9 +13,10 @@ in {
   config = lib.mkIf cfg.enable {
     modules.windowManager = {
       enable = true;
-      default = "hyprland";
+      default = "niri";
 
       hyprland.enable = true;
+      niri.enable = true;
       sway.enable = true;
     };
 
