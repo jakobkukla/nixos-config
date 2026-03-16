d11ef44da5d610af888c8aa3d2dadd684c7db376
jakobkukla <jakob.kukla@gmail.com>
2026-03-16 17:23:48 +0100

    wip dms

---
+++ modules/home/dms/niri.nix
@@ -0,0 +1,21 @@
+{
+  lib,
+  config,
+  osConfig,
+  inputs,
+  ...
+}: let
+  cfg = config.modules.home.dms;
+in {
+  imports = [
+    inputs.dms.homeModules.niri
+  ];
+
+  config = lib.mkIf (cfg.enable && osConfig.modules.windowManager.niri.enable) {
+    programs.dank-material-shell.niri = {
+      includes.enable = false;
+      enableKeybinds = true;
+      enableSpawn = true;
+    };
+  };
+}
