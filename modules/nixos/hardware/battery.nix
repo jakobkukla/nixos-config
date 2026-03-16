115a29f0126b815dfe1c0e28164e586e6ca7d3d6
jakobkukla <jakob.kukla@gmail.com>
2026-03-16 17:15:52 +0100

    feat(profiles/laptop): switch to upower and power-profiles-daemon for power management.
    This is done in preperation for DMS which needs `power-profiles-daemon`.

--- modules/nixos/hardware/battery.nix
+++ modules/nixos/hardware/battery.nix
@@ -6,8 +6,9 @@
   config = lib.mkIf config.device.hardware.battery {
     # Power management and performance scaling
     powerManagement.enable = true;
-    services.auto-cpufreq.enable = true;
     services.thermald.enable = true;
+    services.upower.enable = true;
+    services.power-profiles-daemon.enable = true;
 
     home-manager.users.${config.modules.user.name} = {
       # Battery notification daemon
