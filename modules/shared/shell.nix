9c7c0cca593a5f7d350fde973d89ffb4dd08a54e
jakobkukla <jakob.kukla@gmail.com>
2026-07-22 02:46:28 +0200

    refactor(modules): make shell a shared module.

--- modules/shared/shell.nix
+++ modules/shared/shell.nix
@@ -10,33 +10,37 @@
   };
 
   config = lib.mkIf config.modules.shell.enable {
-    programs.zsh = {
-      enable = true;
-      autosuggestions.enable = true;
-      syntaxHighlighting.enable = true;
-    };
+    programs.zsh.enable = true;
 
     home-manager.users.${config.modules.user.name} = hmArgs: {
       imports = [
         inputs.nix-index-database.homeModules.default
       ];
 
-      home.packages = with pkgs; [
-        nitch
-        onefetch
-        sd
-      ];
+      home.packages = with pkgs;
+        [
+          onefetch
+          sd
+        ]
+        ++ lib.optionals stdenv.hostPlatform.isLinux [
+          nitch
+        ];
 
       programs.zsh = {
         enable = true;
         # Enable new dotDir behaviour (default as of home.stateVersion >= 26.05)
         dotDir = "${hmArgs.config.xdg.configHome}/zsh";
-        shellAliases = {
-          cat = "bat";
-          ip = "ip --color";
-          # rescan wireless APs and open nmtui
-          nmrt = "nmcli device wifi rescan; nmtui";
-        };
+        shellAliases =
+          {
+            cat = "bat";
+          }
+          // lib.optionalAttrs pkgs.stdenv.hostPlatform.isLinux {
+            ip = "ip --color";
+            # rescan wireless APs and open nmtui
+            nmrt = "nmcli device wifi rescan; nmtui";
+          };
+        autosuggestion.enable = true;
+        syntaxHighlighting.enable = true;
         historySubstringSearch = {
           enable = true;
           searchUpKey = "$terminfo[kcuu1]";
