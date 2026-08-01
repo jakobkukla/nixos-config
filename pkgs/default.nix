6231d23841fd8d015587fb4e96665bfcd49f5762
jakobkukla <jakob.kukla@gmail.com>
2026-08-01 17:30:07 +0200

    docs(pkgs): fix example overrides.

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -2,9 +2,9 @@
   # deadnix: skip
   flake.overlays.default = final: prev: {
     # Overrides
-    # old = prev.callPackage ./old.nix;
+    # old = prev.callPackage ./old.nix {};
 
     # New packages
-    # new = final.callPackage ./new.nix;
+    # new = final.callPackage ./new.nix {};
   };
 }
