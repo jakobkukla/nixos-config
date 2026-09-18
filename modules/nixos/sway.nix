948ec07d6f83e0848dcd13a697dab6cd8883e6fc
jakobkukla <jakob.kukla@gmail.com>
2026-09-18 14:45:01 +0200

    feat(sway): remove redundant portal and dbus configuration.

--- modules/nixos/sway.nix
+++ modules/nixos/sway.nix
@@ -29,16 +29,8 @@ in {
       };
     };
 
-    # Enable xdg-desktop-portal
-    services.dbus.enable = true;
-    xdg.portal = {
-      enable = true;
-      wlr.enable = true;
-      # fix xdg-open in FHS or wrappers (see https://github.com/NixOS/nixpkgs/issues/160923)
-      xdgOpenUsePortal = true;
-      # gtk portal needed to make gtk apps happy
-      extraPortals = [pkgs.xdg-desktop-portal-gtk];
-    };
+    # fix xdg-open in FHS or wrappers (see https://github.com/NixOS/nixpkgs/issues/160923)
+    xdg.portal.xdgOpenUsePortal = true;
 
     # Enable real-time capabilities for all programs run by the users group (in particular sway)
     security.pam.loginLimits = [
