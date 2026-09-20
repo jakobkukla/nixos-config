734e005d800776376b09c301069c938353457ade
jakobkukla <jakob.kukla@gmail.com>
2026-09-20 23:56:48 +0200

    feat(sway): add us keyboard and toggle to be consistent with other compositors.

--- modules/nixos/desktop-environment/compositors/sway.nix
+++ modules/nixos/desktop-environment/compositors/sway.nix
@@ -48,8 +48,9 @@ in {
 
           input = {
             "type:keyboard" = {
-              xkb_layout = "de";
-              xkb_variant = "nodeadkeys";
+              xkb_layout = "de,us";
+              xkb_variant = "nodeadkeys,";
+              xkb_options = "grp:ctrls_toggle";
             };
 
             "type:touchpad" = {
